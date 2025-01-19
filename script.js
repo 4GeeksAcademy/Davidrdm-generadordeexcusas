c0d96fad9749fc77db4f6ebcb5fabdc8b876d720
Davidruiz03061976 <davidruizdemanuel39@gmail.com>
2025-01-19 17:03:14 +0000

    Ajustando archivos

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-window.onload=function () {
-    // array with the words
-    let article = [ 'El','Mi'];
-    let adj = [ 'rojo', 'divertido','locura','luminoso', 'brillante'];
-    let noun = ['conductor', 'coche', 'carretera','paisaje','perro'];
-    let action = [ 'tocarme',' robarme','trabajar', 'conducir',' limpiar'];
-    let possetion = [ 'Mi garaje', 'mi trabajo', 'mi casa', 'mi jardin', 'mi calle','mi vecino', 'mi perro'];
-    let where = [ 'en mi calle', 'en mi jardin','en mi camino','cerca de la estacion de bus',' enfrente de la oficina'];
-}
-    // declaring random varibles
-let numarticle = Math.floor(Math.random() * article.length);
-let numadj = Math.floor(Math.random() * adj.length);
-let numnoun = Math.floor(Math.random() * noun.length);
-let numaction = Math.floor(Math.random() * action.length);
-let numpossetion = Math.floor(Math.random() * possetion.length);
-let numwhere = Math.floor(Math.random() * where.length);
+// window.onload = function() {};
+let buttonGenerator = document.querySelector(".buttonGenerator");
+buttonGenerator.addEventListener("click", function() {
+  // array with the words
+  let article = ["El", "Mi"];
+  let adj = ["rojo", "divertido", "locura", "luminoso", "brillante"];
+  let noun = ["conductor", "coche", "carretera", "paisaje", "perro"];
+  let action = ["tocarme", " robarme", "trabajar", "conducir", " limpiar"];
+  let possetion = [
+    "Mi garaje",
+    "mi trabajo",
+    "mi casa",
+    "mi jardin",
+    "mi calle",
+    "mi vecino",
+    "mi perro"
+  ];
+  let where = [
+    "en mi calle",
+    "en mi jardin",
+    "en mi camino",
+    "cerca de la estacion de bus",
+    " enfrente de la oficina"
+  ];
 
-let textExcuse = document.querySelector('.textExcuse');
-let button Generator = document.querySelector('.button Generator');
-if (numarticle == 0 || numnoun == 1){
-    if ( numnoun == 1){
+  // declaring random varibles
+  let numarticle = Math.floor(Math.random() * article.length);
+  let numadj = Math.floor(Math.random() * adj.length);
+  let numnoun = Math.floor(Math.random() * noun.length);
+  let numaction = Math.floor(Math.random() * action.length);
+  let numpossetion = Math.floor(Math.random() * possetion.length);
+  let numwhere = Math.floor(Math.random() * where.length);
 
-// creating a sentences (textExcuse)
-document.querySelector("#textExcuse").innerHTML = `${article[numarticle]} ${adj[numadj]} ${noun[numnoun]} ${action[numaction]} ${possetion[numpossetion]} ${where[numwhere]}`;
+  let textExcuse = document.querySelector(".textExcuse");
+
+  if (numarticle == 0) {
+    if (numnoun == 1) {
+      numarticle = 1;
     }
-buttonGenerate.addEventListener(function(Generator){}// When the button is clicked)
-}
+  }
+  // creating a sentences (textExcuse)
+
+  textExcuse.innerHTML = `${article[numarticle]} ${adj[numadj]} ${noun[numnoun]} ${action[numaction]} ${possetion[numpossetion]} ${where[numwhere]}`;
+});
